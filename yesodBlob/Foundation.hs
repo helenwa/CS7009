6972f4dea8b709b5e3c4452512874fa2f88a9d21
helenwa <wallach@tcd.ie>
2017-02-03 11:08:53 +0000

    Restart with yesod-minimal code
    not building..

--- yesodBlob/Foundation.hs
+++ yesodBlob/Foundation.hs
@@ -1,73 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TypeFamilies      #-}
+{-# LANGUAGE ViewPatterns      #-}
 module Foundation where
 
-import Import.NoFoundation
-import Text.Hamlet                 (hamletFile)
-import Text.Jasmine                (minifym)
-import Yesod.Core.Types            (Logger)
-import Yesod.Default.Util          (addStaticContentExternal)
-import qualified Yesod.Core.Unsafe as Unsafe
-import qualified Data.CaseInsensitive as CI
-import qualified Data.Text.Encoding as TE
+import Yesod.Core
 
--- | The foundation datatype for your application. This can be a good place to
--- keep settings and values requiring initialization before your application
--- starts running, such as database connections. Every handler will have
--- access to the data present here.
 data App = App
-    { httpManager    :: Manager
-    }
 
-data MenuItem = MenuItem
-    { menuItemLabel :: Text
-    , menuItemRoute :: Route App
-    , menuItemAccessCallback :: Bool
-    }
+mkYesodData "App" $(parseRoutesFile "routes")
 
-data MenuTypes
-    = NavbarLeft MenuItem
-    | NavbarRight MenuItem
-
--- This is where we define all of the routes in our application. For a full
--- explanation of the syntax, please see:
--- http://www.yesodweb.com/book/routing-and-handlers
---
--- Note that this is really half the story; in Application.hs, mkYesodDispatch
--- generates the rest of the code. Please see the following documentation
--- for an explanation for this split:
--- http://www.yesodweb.com/book/scaffolding-and-the-site-template#scaffolding-and-the-site-template_foundation_and_application_modules
---
--- This function also generates the following type synonyms:
--- type Handler = HandlerT App IO
--- type Widget = WidgetT App IO ()
-mkYesodData "App" $(parseRoutesFile "config/routes")
-
--- | A convenient synonym for creating forms.
-type Form x = Html -> MForm (HandlerT App IO) (FormResult x, Widget)
-
--- Please see the documentation for the Yesod typeclass. There are a number
--- of settings which can be configured by overriding methods here.
-instance Yesod App where
-    --ADDED
-    approot = ApprootStatic "http://localhost:3000"
-    --END ADDED
-
-instance YesodAuth App where
-    type AuthId App = Text
-    getAuthId = return . Just . credsIdent
-
-    loginDest _ = HomeR
-    logoutDest _ = HomeR
-
-    authPlugins _ =
-        [ oauth2GithubScoped (githubToken master)
-		                     (githubSecret master)
-							 ["user,repo,gist"]
-        ]
-
-    authHttpManager = httpManager
-
-    -- The default maybeAuthId assumes a Persistent database. We're going for a
-    -- simpler AuthId, so we'll just do a direct lookup in the session.
-    maybeAuthId = lookupSession "_ID"
-instance RenderMessage App FormMessage where
-    renderMessage _ _ = defaultFormMessage
+instance Yesod App
