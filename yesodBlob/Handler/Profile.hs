98ad5c2be2a1fdd72573531439b931dd7e56f1bd
helenwa <wallach@tcd.ie>
2017-02-08 17:08:26 +0000

    Getting repos and stars for user
    Need to work out incorperating auth.
    Stared to use show instead of fn to good effect
    todo use d3 to make json something

--- yesodBlob/Handler/Profile.hs
+++ yesodBlob/Handler/Profile.hs
@@ -1,24 +1,50 @@
 module Handler.Profile where
 
-import Import
-import qualified GitHub
+import Import hiding (unpack, pack, Auth)
+import Data.List hiding(intercalate, map, lookup)
+import GitHub
+import GitHub.Data.Repos
+import GitHub.Endpoints.Repos
+--import GitHub.Data.Activities
+import GitHub.Endpoints.Activity.Starring
+import Data.Maybe
+import Data.Text.Encoding
+import Data.Text hiding(intercalate, map, lookup)
+import Data.Map hiding(intercalate, map, lookup)
 
 getProfileR :: Handler Html 
 getProfileR = do
+
     (_, user) <- requireAuthPair
-    
     sess <- getSession
     let log = lookup "login" sess
     let token = lookup "access_token" sess
+    let textName = Data.Text.Encoding.decodeUtf8 (fromJust log)
+    repositorys <- liftIO $ repos textName
+    stared <- liftIO $ stars textName Nothing 
+    let repNo = Data.List.length repositorys
     defaultLayout $ do
         setTitle . toHtml $ userIdent user  <> "'s User page"
         $(widgetFile "profile")
 
-    
-    
-    --possibleUser <- GitHub.executeRequest' $ GitHub.userInfoCurrentR
-    --let possibleUser = GitHub.userInfoCurrentR
-    --let possibleUser = GitHub.userInfoCurrentR
-    --case possibleUser of
-    ----   (Left error)  -> putStrLn $ "Error: " ++ (show error)
-    --   (Right user) -> putStrLn $ intercalate "\n\n" $ user
+--Get users repositorys   
+repos :: Text -> IO (String)
+repos userName = do
+  possibleRepos <- GitHub.Endpoints.Repos.userRepos (mkOwnerName userName) GitHub.Data.Repos.RepoPublicityAll
+  case possibleRepos of
+       (Left error)  -> do 
+            return $ "Error: " Data.List.++ (show error)
+            
+       (Right repos) -> do 
+            return $ intercalate "\n\n" $ map show repos
+            
+--Get repositorys stared by user
+stars :: Text -> Maybe Auth-> IO (String)
+stars userName auth = do
+  starRepos <- GitHub.Endpoints.Activity.Starring.reposStarredBy auth (mkOwnerName userName) --GitHub.Data.Repos.RepoPublicityAll
+  case starRepos of
+       (Left error)  -> do 
+            return $ "Error: " Data.List.++ (show error)
+       (Right repos) -> do 
+            return $ intercalate "\n\n" $ map show repos
+ 
